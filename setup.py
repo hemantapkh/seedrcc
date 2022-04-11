1123f290d06a660dac1b993ce326deba117ba3b5
Hemanta Pokharel <hemantapkh@yahoo.com>
2022-04-11 17:46:38 +0545

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="seedrcc", 
+    version="0.0.1",
+    author="Hemanta Pokharel",
+    author_email="hemantapkh@yahoo.com",
+    description="Unofficial Python API Seedr.cc",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=["requests"],
+    url="https://github.com/hemantapkh/seedrcc",
+    project_urls={
+        "Issue tracker": "https://github.com/hemantapkh/seedrcc/issues",
+      },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    python_requires='>=3.0',
+)
