b2322816b8f4a3e4885bdb8ac7b209ed61c75017
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-07-25 18:45:07 +0545

    Add Token dataclass and helper function for delete items

--- seedrcc/__init__.py
+++ seedrcc/__init__.py
@@ -1,4 +1,5 @@
+from ._token import Token
 from .exceptions import APIError, AuthenticationError, SeedrError
 from .seedr import Seedr
 
-__all__ = ["Seedr", "SeedrError", "APIError", "AuthenticationError"]
+__all__ = ["Seedr", "Token", "SeedrError", "APIError", "AuthenticationError"]
